9b716ee9574653dc551e694c33473842911a7b82
fabiohanada23 <61828534+fabiohanada23@users.noreply.github.com>
2025-11-27 21:02:40 -0300

    aula 22/156

---
+++ genres/permissions.py
@@ -0,0 +1,9 @@
+from rest_framework import permissions
+
+
+class GenrePermissionClass(permissions.BasePermission):
+    
+    def has_permission(self, request, view):
+        #logica
+        
+        return True

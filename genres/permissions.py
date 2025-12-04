ad201444b8bacffc9c59af8e653df30eb729d2df
fabiohanada23 <61828534+fabiohanada23@users.noreply.github.com>
2025-12-03 21:20:11 -0300

    aula 23/161

--- genres/permissions.py
+++ genres/permissions.py
@@ -5,5 +5,16 @@ class GenrePermissionClass(permissions.BasePermission):
     
     def has_permission(self, request, view):
         #logica
+        if request.method in ['GET', 'OPTIONS', 'HEAD']:
+            return request.user.has_perm('genres.view_genre')
         
-        return True
+        if request.method == 'POST':
+            return request.user.has_perm('genres.add_genre')
+        
+        if request.method in ['PATCH', 'PUT']:
+            return request.user.has_perm('genres.change_genre')
+        
+        if request.method == 'DELETE':
+            return request.user.has_perm('genres.delete_genre')
+        
+        return False
